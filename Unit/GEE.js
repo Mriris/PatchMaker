50447892d7f72d346abfebb5b75424e34f2abd2b
Mriris <2647381485@qq.com>
2025-01-13 15:08:28 +0800

    分片下载

--- Unit/GEE.js
+++ Unit/GEE.js
@@ -1,40 +1,64 @@
-var startDate = ee.Date('2022-01-01'); // set start time for analysis
-var endDate = ee.Date('2022-12-31'); // set end time for analysis
-var nDays = ee.Number(endDate.difference(startDate,'day')).round();
-// coordinate Arpat Ponte alle Mosse
-//var point = ee.Geometry.Point([11.230520538799576, 43.78480193916678]);
-// Signa 11.097849 43.793234
-//https://www.arpat.toscana.it/temi-ambientali/aria/qualita-aria/rete_monitoraggio/scheda_stazione/FI-SIGNA
-var point = ee.Geometry.Point([11.097849, 43.793234]);
-Map.setCenter(11.097849, 43.793234,14);
-var chirps = ee.ImageCollection('COPERNICUS/S5P/NRTI/L3_O3')
-            .select('O3_column_number_density')
-            .filterBounds(point)
-            .filterDate(startDate, endDate)
-            .map(function(image){return image.clip(point)}) ;
+// 定义感兴趣区域（ROI）
+var roi = ee.Geometry.Rectangle([
+  384100.6489999999757856, 4319736.0499999998137355, // 左下角
+  414580.6489999999757856, 4346080.0499999998137355  // 右上角
+]);
 
-//这个关键地方，，是需要我们建立一个时序，然后获取每一天的值，这里最主要的时间函数的运用，以及影像系统时间的设定
-var byday = ee.ImageCollection(
-  // map over each day
-  ee.List.sequence(0,nDays).map(function (n) {
-    var ini = startDate.advance(n,'day');
-    // advance just one day
-    var end = ini.advance(1,'day');
-    return chirps.filterDate(ini,end)
-                .select(0).mean()
-                .set('system:time_start', ini);
-}));
-// plot full time series
-print(
-  ui.Chart.image.series({
-    imageCollection: byday,
-    region: point,
-    scale: 1
-  }).setOptions({title: 'O3 Signa 2020 mol/m2'})
-);
-Export.table.toDrive({
-        collection: chirps,
-        description: 'O3',
-    fileNamePrefix: 'O3',
-    fileFormat: 'CSV'
-    });
+// 获取 ROI 的边界坐标
+var bounds = roi.bounds().coordinates().get(0).getInfo(); // 使用 getInfo() 将 ee.List 转换为 JavaScript 数组
+var xMin = bounds[0][0]; // 左下角经度
+var yMin = bounds[0][1]; // 左下角纬度
+var xMax = bounds[2][0]; // 右上角经度
+var yMax = bounds[2][1]; // 右上角纬度
+
+// 定义分块数
+var numX = 4; // 水平方向分为 4 块
+var numY = 4; // 垂直方向分为 4 块
+
+// 计算每块的宽度和高度
+var xStep = (xMax - xMin) / numX;
+var yStep = (yMax - yMin) / numY;
+
+// Sentinel-2 数据
+var collection = ee.ImageCollection('COPERNICUS/S2')
+                  .filterBounds(roi)
+                  .filterDate('2018-03-01', '2018-04-30')
+                  .sort('system:time_start');
+
+// 获取最接近目标日期的影像
+var targetDate = ee.Date('2018-03-31');
+var closestImage = collection.map(function(image) {
+  var diff = image.date().difference(targetDate, 'day').abs();
+  return image.set('time_diff', diff);
+}).sort('time_diff').first();
+
+// 提取 RGB 波段
+var rgbImage = closestImage.select(['B4', 'B3', 'B2']); // 红、绿、蓝波段
+
+// 定义导出函数
+function exportTile(i, j) {
+  var x0 = xMin + xStep * i;       // 块的左边界
+  var x1 = x0 + xStep;             // 块的右边界
+  var y0 = yMin + yStep * j;       // 块的下边界
+  var y1 = y0 + yStep;             // 块的上边界
+
+  var tile = ee.Geometry.Rectangle([x0, y0, x1, y1]); // 定义子块
+  var clippedTile = rgbImage.clip(tile);              // 裁剪影像
+
+  Export.image.toDrive({
+    image: clippedTile,
+    description: 'Sentinel2_20180331_tile_' + i + '_' + j,
+    scale: 10,
+    region: tile,
+    crs: 'EPSG:32633', // 替换为您需要的坐标系
+    fileFormat: 'GeoTIFF',
+    maxPixels: 1e13
+  });
+}
+
+// 遍历所有子块并导出
+for (var i = 0; i < numX; i++) {
+  for (var j = 0; j < numY; j++) {
+    exportTile(i, j);
+  }
+}
